6958d354b206e844a821308ed109d2d25b3f7500
Amanve77 <amanve.bsp07@gmail.com>
2024-08-31 16:10:05 +0530

    correction in q29

--- Hands on List 1/29.c
+++ Hands on List 1/29.c
@@ -12,6 +12,7 @@ Date: 31th Aug, 2024.
 #include <sys/types.h> 
 #include <unistd.h>    
 #include <stdio.h>     
+#include <stdlib.h> 
 
 int main()
 {
@@ -19,41 +20,65 @@ int main()
     pid_t pid;
     pid = getpid();
     currentPolicy = sched_getscheduler(pid);
+    if(currentPolicy == -1) 
+    {
+        perror("sched_setscheduler");
+        exit(0);
+    }
     struct sched_param priority;
     
     priority.sched_priority = 10;
 
     switch (currentPolicy)
     {
-    case SCHED_FIFO:
-        printf("Current policy is FIFO\n");
-        if(sched_setscheduler(pid, SCHED_RR, &priority) == -1)
-            perror("sched_setscheduler");
-        currentPolicy = sched_getscheduler(pid);
-        if(currentPolicy == -1)
-            perror("sched_getscheduler");
-        printf("Current policy is %d\n", currentPolicy);
-        break;
-    case SCHED_RR:
-        printf("Current policy is RR\n");
-        if(sched_setscheduler(pid, SCHED_FIFO, &priority) == -1)
-            perror("sched_setscheduler");
-        currentPolicy = sched_getscheduler(pid);
-        if(currentPolicy == -1)
-            perror("sched_getscheduler");
-        printf("Current policy is %d\n", currentPolicy);
-        break;
-    case SCHED_OTHER:
-        printf("Current policy is OTHER\n");
-        if(sched_setscheduler(pid, SCHED_RR, &priority) == -1)
-            perror("sched_setscheduler");
-        currentPolicy = sched_getscheduler(pid);
-        if(currentPolicy == -1)
-            perror("sched_getscheduler");
-        printf("Current policy is %d\n", currentPolicy);
-        break;
-    default:
-        perror("Error while getting current policy\n");
+        case SCHED_FIFO:
+            printf("Current policy is FIFO\n");
+            if(sched_setscheduler(pid, SCHED_RR, &priority) == -1)
+            {
+                perror("sched_setscheduler");
+                exit(0);
+            }
+            break;
+        case SCHED_RR:
+            printf("Current policy is RR\n");
+            if(sched_setscheduler(pid, SCHED_FIFO, &priority) == -1)
+            {
+                perror("sched_setscheduler");
+                exit(0);
+            }
+            break;
+        case SCHED_OTHER:
+            printf("Current policy is OTHER\n");
+            if(sched_setscheduler(pid, SCHED_RR, &priority) == -1)
+            {
+                perror("sched_setscheduler");
+                exit(0);
+            }
+            break;
+        default:
+            perror("Error while getting current policy\n");
+    }
+
+    currentPolicy = sched_getscheduler(pid);
+    if(currentPolicy == -1) 
+    {
+        perror("sched_getscheduler");
+        exit(0);
+    }
+    switch(currentPolicy) 
+    {
+        case SCHED_FIFO:
+            printf("New policy is SCHED_FIFO\n");
+            break;
+        case SCHED_RR:
+            printf("New policy is SCHED_RR\n");
+            break;
+        case SCHED_OTHER:
+            printf("New policy is SCHED_OTHER\n");
+            break;
+        default:
+            printf("New policy is unrecognized (%d)\n", currentPolicy);
+            break;
     }
 
     return 0;
@@ -63,6 +88,6 @@ int main()
 ============================================================================
 Output:
     Current policy is OTHER
-    Current policy is 2
+    New policy is SCHED_RR
 ============================================================================
 */
